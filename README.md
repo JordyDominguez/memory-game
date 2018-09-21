ee7ec6d0ce8d7f4ce8476fc12568197e229ccea9
Jordy Dominguez <jdomi1106@gmail.com>
2018-09-20 21:20:38 -0400

    Memory Game Project
    Udacity Memory Game

---
+++ README.md
@@ -0,0 +1,9 @@
+# Memory Game Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Instructions
+
+Click on two cards to try to match the symbols. If they do not match the cards will shake red and then flip back over. If the cards do match the cards will pulse blue and the cards will remain out. Match all of the cards to win. Make sure to use the least amount of moves possible to keep your stars! 
---
+++ js/app.js
@@ -0,0 +1,210 @@
+// List that holds all cards
+const cards = ['fa-diamond', 'fa-diamond',
+			   'fa-paper-plane-o', 'fa-paper-plane-o',
+			   'fa-anchor', 'fa-anchor',
+			   'fa-bolt', 'fa-bolt',
+			   'fa-cube', 'fa-cube',
+			   'fa-leaf', 'fa-leaf',
+			   'fa-bicycle', 'fa-bicycle',
+			   'fa-bomb', 'fa-bomb'
+			  ];
+
+//Function that creates the cards
+function generateCard(card){
+	return`<li class="card" data-card="${card}"> <i class="fa ${card}"></i></li>`;
+}
+
+
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+let movesMade = 0;
+const moveCounter = document.querySelector('.moves');
+
+let timerStart = false;
+
+
+let timerMin = document.querySelector('.minutes');
+let timerSec = document.querySelector('.seconds');
+let min = 0;
+let sec = 0;
+let timerCount = null;
+
+let starCount = 3;
+let starRating = [];
+
+let openCards = [];
+let cardsMatched = 0;
+
+let endGame = false;
+
+
+
+//Get Modal element
+const modal = document.querySelector('.modal');
+//Get Modal Play Again Button
+const tryAgain = document.querySelector('.play-again');
+//Get Modal Stars Class
+const finalStars = document.querySelector('.final-stars');
+//Get Modal Time Class
+const finalTime = document.querySelector('.time');
+//Get Modal Moves Class
+const finalMoves = document.querySelector('.final-moves');
+
+//Creates board and shuffles cards
+function createBoard(){
+	const deck = document.querySelector('.deck');
+ 	const cardHTML = shuffle(cards).map(function(card){
+ 		return generateCard(card);
+ 	});
+
+ 	deck.innerHTML = cardHTML.join('');
+
+	moveCounter.innerText = movesMade;
+}
+
+
+//Compares cards to eachother and if all cards are matched runs gameOver
+function compareCards() {
+	if(openCards.length == 2) {
+
+		starSystem();
+
+		if (openCards[0].dataset.card == openCards[1].dataset.card) {
+			openCards[0].classList.add('match');
+			openCards[1].classList.add('match');
+			openCards = [];
+			cardsMatched ++;
+			if (cardsMatched == 8) {
+ 				gameOver();
+ 			}
+		} else {
+			openCards[0].classList.add('wrong');
+			openCards[1].classList.add('wrong');
+			setTimeout(function(){
+				removeCards();
+			}, 1000);
+		}
+
+		counter();
+	}
+
+}
+
+// Hides card if they do not match
+function removeCards() {
+		openCards.forEach(function(card){
+			card.classList.remove('open', 'show', 'wrong');
+			openCards = [];
+		});
+}
+
+//Counts how many moves are made and converts to text for html
+function counter() {
+ 	movesMade += 1;
+ 	moveCounter.innerText = movesMade;
+}
+
+// Grabs star for Star Rating system
+const stars = document.querySelectorAll('ul.stars li');
+
+stars.forEach(function(star){
+ 	starRating.push(star);
+});
+
+// Function that manages the star rating system
+ function starSystem() {
+ 	if (movesMade == 10) {
+ 		starRating[2].style.visibility = "hidden";
+ 		starCount --;
+ 	} else if (movesMade == 14) {
+ 		starRating[1].style.visibility = "hidden";
+ 		starCount --;
+ 	}
+ }
+
+//End Game Function to Open Modal
+function openModal() {
+ 	modal.style.display = "flex";
+ 	finalStars.innerHTML += `Stars Rating: ${starCount} out of 3`;
+ 	finalTime.innerHTML += `Time: ${min}:${sec}`;
+ 	finalMoves.innerHTML += `Moves: ${movesMade}`;
+}
+
+
+// Function that allows you to click the card, runs the function to compare cards, and starts the timer on the first click
+function clickCard() {
+	const selectCard = document.querySelectorAll('.card');
+	selectCard.forEach(function(card){
+		card.addEventListener('click', function(e){
+			if (openCards.length < 2){
+				if (!card.classList.contains ('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
+					card.classList.add('open', 'show');
+					openCards.push(card);
+					compareCards();
+					timer();
+				}
+			}
+		});
+ 	});
+}
+//Function that runs the timer
+function timer() {
+	if (timerStart == false){
+	 	timerStart = true;
+		timerCount = setInterval(function () {
+			sec ++;
+			if (sec < 60){
+				timerSec.innerText = sec.toLocaleString(undefined,{minimumIntegerDigits: 2});
+			} else{
+				sec = 0;
+				min ++;
+				timerSec.innerText = sec.toLocaleString(undefined,{minimumIntegerDigits: 2});
+				timerMin.innerText = min.toLocaleString(undefined,{minimumIntegerDigits: 2});
+				}
+		}, 1000);
+	}
+}
+
+// Function to end the game, stop the timer, and open the modal pop up
+function gameOver() {
+ 	endGame = true;
+ 	stopTimer();
+	openModal();
+}
+
+//Function to Stop Timer
+function stopTimer(){
+	clearInterval(timerCount);
+}
+
+ //Restart Game
+ const restartButton = document.querySelector('.restart');
+ restartButton.addEventListener('click', function() {
+ 	document.location.reload();
+ });
+
+//Try again button
+ tryAgain.addEventListener('click', function() {
+ 	document.location.reload();
+ });
+
+createBoard();
+clickCard();
+
+
+
